7789ebca9d231b840ed4bac2efa0608e559c64dd
Niyaz <niyaz@jocom.my>
2019-02-28 17:35:43 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <!-- Apollo Query -->
+    <ApolloQuery :query="require('@/graphql/users.gql')">
+      <!-- The result will automatically updated -->
+      <template slot-scope="{ result: { data, loading } }">
+        <!-- Some content -->
+        <div v-if="loading">Loading...</div>
+        <ul v-else>
+          <li v-for="user of data.users" class="user">
+            {{ user.name }}
+          </li>
+        </ul>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import gql from 'graphql-tag'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+        categories: []
+    }
+  },
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    categories: gql`query {
+      categories {
+        id,
+        title
+      }
+    }`,
+  },
 }
 </script>
